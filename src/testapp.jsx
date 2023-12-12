f410192ae9216a51caf26ff0265167e987d1c30e
AresTechnology <arestechnology.dev@gmail.com>
2023-12-12 23:57:08 +0100

    boilerplate + some tests

---
+++ src/testapp.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+export default function Test() {
+    
+    
+    return (
+        <div>
+            <a href="./startserv/starter.html">
+            <p className="idk">Testowy przenośnik</p>
+            </a>
+        </div>
+    )
+}
