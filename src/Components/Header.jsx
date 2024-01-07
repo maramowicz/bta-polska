85a7b4c2e91feb53b86dd0c0c304cdc6eb7900de
AresTechnology <arestechnology.dev@gmail.com>
2024-01-08 00:20:30 +0100

    naprawa jebanego asseta3

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import '/src/index.css'
 import '/src/App.css'
-import BTApl from '/src/assets/BTApl.jpg'
+
 
 
 export function Header() {
     return (
    
             <nav className='navbar'>
-                <img className='logo-navbar' src={BTApl}></img>
+                <img className='logo-navbar' src='/BTApl.jpg'></img>
                 <ul className='pages-navbar'>
                     <li className='page'>Home </li>
                     <li className='page'>Server</li>
